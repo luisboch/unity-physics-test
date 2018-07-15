62ca3c92ea25e74f0b056432e4c6ccb3f730611d
Luis Carlos Boch <luis.c.boch@gmail.com>
2018-07-15 16:25:09 -0300

    Adicao script base para os dispositivos (armas e defesas)

---
+++ TestShip/Assets/Scripts/devices/DeviceBase.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class DeviceBase : MonoBehaviour {
+
+    public int ammunitionQuantity;
+
+
+    [Header("Tempo para a recarga (em milisegundos)")]
+    public long reloadTime;
+}
